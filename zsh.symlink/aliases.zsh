369d14e71b156010e927bb181dca4460e58c2c21
Christoph Schiessl <cs@proactive.cc>
2016-03-10 15:39:49 +0100

    zsh: alias git to hub if available (hub = github cli interface)

--- zsh.symlink/aliases.zsh
+++ zsh.symlink/aliases.zsh
@@ -3,6 +3,9 @@ alias ll="ls -Glh --color"
 alias la="ls -Glha --color"
 alias grep="grep --color --line-number"
 
+# CLI interface for GitHub installed?
+exists "hub" && eval "$(hub alias -s)"
+
 exists "git" && alias g="git"
 exists "task" && alias t="task"
 exists "pass" && alias p="pass"
