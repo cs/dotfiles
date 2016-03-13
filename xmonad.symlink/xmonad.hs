052a268f2d4a0031247166b9a16f0f1cf19902a7
Christoph Schiessl <cs@proactive.cc>
2016-03-13 16:17:16 +0100

    xmonad: use urxvt as terminal

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -2,3 +2,8 @@ import XMonad
 
 main :: IO ()
 main = xmonad defaultConfig
+    { terminal = "urxvt"
+    , borderWidth = 3
+    , normalBorderColor = "#333333"
+    , focusedBorderColor = "#00ff00"
+    }
