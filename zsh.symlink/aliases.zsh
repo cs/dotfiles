96599049cfa4b800df7df325987482160f297b07
Christoph Schiessl <cs@proactive.cc>
2016-03-13 15:29:05 +0100

    less: export LESSHISTFILE env var

--- zsh.symlink/aliases.zsh
+++ zsh.symlink/aliases.zsh
@@ -3,10 +3,6 @@ alias ll="ls -Glh --color"
 alias la="ls -Glha --color"
 alias grep="grep --color --line-number"
 
-if exists "less"; then
-  alias less="LESSHISTFILE=$HOME/.histories/less less"
-fi
-
 # CLI interface for GitHub installed?
 exists "hub" && eval "$(hub alias -s)"
 
--- zsh.symlink/base.zsh
+++ zsh.symlink/base.zsh
@@ -5,3 +5,13 @@ setopt IGNORE_EOF
 function exists {
   command -v "$1" > /dev/null
 }
+
+# Use vim as the default editor:
+if exists "vim"; then
+  export EDITOR=vim
+fi
+
+# Store less history in a custom file:
+if exists "less"; then
+  export LESSHISTFILE=$HOME/.histories/less
+fi
