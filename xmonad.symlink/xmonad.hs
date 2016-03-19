cb5d357e76a1f3ffbd81638e1394c82529d19ac0
Christoph Schiessl <cs@proactive.cc>
2016-03-19 13:05:52 +0100

    xmonad: explicitly define workspaces

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,9 +1,12 @@
 import XMonad
 
 main :: IO ()
-main = xmonad defaultConfig
+main = do
+  xmonad defaultConfig
     { terminal = "urxvt"
     , borderWidth = 3
     , normalBorderColor = "#333333"
     , focusedBorderColor = "#00ff00"
+    , modMask = mod1Mask
+    , workspaces = fmap show [1..9]
     }
