ad2fb7c83ce1dc1b82208d9fb36491c25bdfdda3
Christoph Schiessl <cs@proactive.cc>
2016-03-22 18:07:54 +0100

    xmonad: add layoutHook

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,4 +1,7 @@
 import XMonad
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Spacing
 
 main :: IO ()
 main = do
@@ -9,4 +12,15 @@ main = do
     , focusedBorderColor = "#00ff00"
     , modMask = mod1Mask
     , workspaces = fmap show [1..9]
+    , layoutHook = smartBorders $ awesomeLayout
     }
+
+awesomeLayout = tiled ||| Mirror tiled ||| Full
+  where
+    tiled = spacing 10 $ ResizableTall nmaster delta ratio []
+    -- Default number of windows in master pane
+    nmaster = 1
+    -- Percent of the screen to increment when resizing
+    delta = 5/100
+    -- Default proportion of the screen taken up by main pane
+    ratio = toRational (2/(1 + sqrt 5 :: Double))
