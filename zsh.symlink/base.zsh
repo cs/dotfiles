0ec2012a4bbaa330f3aed5fea419e02937dc629e
Christoph Schiessl <cs@proactive.cc>
2018-08-14 16:55:52 +0200

    zsh: use nvim as the $EDITOR

--- zsh.symlink/base.zsh
+++ zsh.symlink/base.zsh
@@ -6,12 +6,8 @@ function exists {
   command -v "$1" > /dev/null
 }
 
-# Use vim as the default editor:
-if exists "vim"; then
-  export EDITOR=vim
-fi
+# Use nvim as the default editor:
+exists "nvim" && export EDITOR=nvim
 
 # Store less history in a custom file:
-if exists "less"; then
-  export LESSHISTFILE=$HOME/.histories/less
-fi
+exists "less"; export LESSHISTFILE=$HOME/.histories/less
